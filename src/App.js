299685c9580c6701400fc93206bfe801abeb14dc
RoarInTech <dev.tirthesh@gmail.com>
2023-02-04 15:01:13 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Box from './Components/Box';
 
-function App() {
+function App() {    
   return (
     <div className="App">
-      <h1>Hello World!!!</h1>
+      <h1>My First React App</h1>  
+      <hr></hr>
+      <Box></Box>
     </div>
   );
 }
---
+++ src/Components/Box.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const box = () => {
+ return(
+    <div className="box">
+       Test 
+    </div>
+ )
+};
+
+export default box;
